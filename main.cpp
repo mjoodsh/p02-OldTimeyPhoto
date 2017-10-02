c0837dcf719e300a1420df7ed5a8de14c5ae7441
mjoodsh <malshikhhussain@mail.csuhcio.edu>
2017-10-02 16:50:50 -0700

    Added functional decpomposition to the file

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "bitmap.h"
+#include <vectors>
+#include <string>
+
+
+int main()
+{
+/* -The program starts by asking the user about the image file which is in BMP format.
+ * -The program will store the file.
+ * -The program will convert the image pixel to a grescale.
+ * -It will save the converted file as oldtimey.bmp
+ *  */
+  return 0;
+}
