4a9811faf3015ce5dcf9663487a873aef1b4fbec
mjoodsh <malshikhhussain@mail.csuhcio.edu>
2017-10-04 22:25:28 -0700

    Opening a bitmap image, converting it to a pixel matrix, and getting the size

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include "bitmap.h"
-#include <vectors>
+#include <vector>
 #include <string>
 
+using namespace std;
 
 int main()
 {
-/* -The program starts by asking the user about the image file which is in BMP format.
+
+ Bitmap image;
+ vector <vector <Pixel> > bmp;
+
+ image.open("machupicchu.bmp");
+ bmp = image.toPixelMatrix();
+
+cout<<"machupicchu.bmp has been loaded. It is "<<bmp[0].size()<<" pixels wide and "<<bmp.size()<<" Pixels high"<<endl;
+
+  /* -The program starts by asking the user about the image file which is in BMP format.
  * -The program will store the file.
  * -The program will convert the image pixel to a grescale.
  * -It will save the converted file as oldtimey.bmp
